65a2bd1e6505037c4e63157ccd1bdc8291f15b5a
ckacy01 <ckacy01@protonmail.com>
2025-10-16 10:45:05 -0600

    Inital commit: Creating the project structure

---
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/MeliECommerceApplication.java
@@ -0,0 +1,13 @@
+package org.technoready.meliecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeliECommerceApplication.class, args);
+    }
+
+}
---
+++ Meli-E-commerce/src/test/java/org/technoready/meliecommerce/MeliECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.technoready.meliecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
