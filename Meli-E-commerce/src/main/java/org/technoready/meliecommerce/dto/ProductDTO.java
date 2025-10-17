a6b94d3a4649068c09e5b7baed9c81599ddf2030
ckacy01 <ckacy01@protonmail.com>
2025-10-17 09:55:55 -0600

    feat: Creating the DTO for a product

---
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/dto/ProductDTO.java
@@ -0,0 +1,21 @@
+package org.technoready.meliecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private LocalTime created_at;
+    private boolean active;
+}
