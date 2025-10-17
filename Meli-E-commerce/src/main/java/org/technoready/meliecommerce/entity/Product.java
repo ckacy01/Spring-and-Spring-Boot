695cf93dd0c145d1b156c9b2e4d378ca12caf6ab
ckacy01 <ckacy01@protonmail.com>
2025-10-17 09:55:38 -0600

    feat: Creating the entity of a product

---
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/entity/Product.java
@@ -0,0 +1,32 @@
+package org.technoready.meliecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+
+import java.time.LocalTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    @CreationTimestamp
+    private LocalTime created_at;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean active = true;
+
+}
