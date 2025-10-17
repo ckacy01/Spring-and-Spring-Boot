04c275fc6308fa3aeb82333da546753e6ef8b91e
ckacy01 <ckacy01@protonmail.com>
2025-10-17 09:56:29 -0600

    feat: Adding the permitted requests

---
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/config/WebConfiguration.java
@@ -0,0 +1,13 @@
+package org.technoready.meliecommerce.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
