4ec5de3628a61a48d37d8cc1d98397587a6c09b7
ckacy01 <ckacy01@protonmail.com>
2025-10-18 16:42:55 -0600

    refactor: improving the messages that the user receives to have more understanding and also a logger info

--- Meli-E-commerce/src/main/java/org/technoready/meliecommerce/controller/ProductController.java
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/controller/ProductController.java
@@ -1,8 +1,11 @@
 package org.technoready.meliecommerce.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.technoready.meliecommerce.dto.SuccessResponseDTO;
 import org.technoready.meliecommerce.entity.Product;
 import org.technoready.meliecommerce.service.ProductService;
 
@@ -11,36 +14,100 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
 
     @GetMapping
-    public List<Product> findAll(@RequestParam(required = false, defaultValue = "true") boolean activeOnly) {
-        if(activeOnly)
-            return productService.findAllByIsActiveTrue();
-    return productService.findAll();
+    public ResponseEntity<SuccessResponseDTO<List<Product>>> findAll(
+            @RequestParam(required = false, defaultValue = "true") boolean activeOnly) {
+
+        log.info("Controller: Received request to get all products (activeOnly: {})", activeOnly);
+
+        List<Product> products = activeOnly
+                ? productService.findAllByIsActiveTrue()
+                : productService.findAll();
+
+        String message = activeOnly
+                ? String.format("Retrieved %d active products successfully", products.size())
+                : String.format("Retrieved %d products successfully", products.size());
+
+        SuccessResponseDTO<List<Product>> response = SuccessResponseDTO.of(
+                HttpStatus.OK.value(),
+                message,
+                products
+        );
+
+        log.info("Controller: Retrieved {} products", products.size());
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}")
-    public Product findById(@PathVariable long id) {
-        return productService.findById(id);
+    public ResponseEntity<SuccessResponseDTO<Product>> findById(@PathVariable long id) {
+        log.info("Controller: Received request to get product {}", id);
+
+        Product product = productService.findById(id);
+
+        SuccessResponseDTO<Product> response = SuccessResponseDTO.of(
+                HttpStatus.OK.value(),
+                String.format("Product %d retrieved successfully", id),
+                product
+        );
+
+        log.info("Controller: Product {} retrieved successfully", id);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
+    public ResponseEntity<SuccessResponseDTO<Void>> delete(@PathVariable Long id) {
+        log.info("Controller: Received request to delete product {}", id);
+
         productService.delete(id);
-        return ResponseEntity.noContent().build();
+
+        SuccessResponseDTO<Void> response = SuccessResponseDTO.of(
+                HttpStatus.OK.value(),
+                String.format("Product %d has been successfully deactivated", id)
+        );
+
+        log.info("Controller: Product {} deleted successfully", id);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping
-    public ResponseEntity<Product> save(@RequestBody Product product) {
-        return ResponseEntity.ok(productService.save(product));
+    public ResponseEntity<SuccessResponseDTO<Product>> save(@RequestBody Product product) {
+        log.info("Controller: Received request to create product {}", product.getName());
+
+        Product savedProduct = productService.save(product);
+
+        SuccessResponseDTO<Product> response = SuccessResponseDTO.of(
+                HttpStatus.CREATED.value(),
+                String.format("Product '%s' created successfully with ID: %d",
+                        savedProduct.getName(), savedProduct.getId()),
+                savedProduct
+        );
+
+        log.info("Controller: Product {} created successfully", savedProduct.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
-        return ResponseEntity.ok(productService.update(product, id));
+    public ResponseEntity<SuccessResponseDTO<Product>> update(
+            @PathVariable Long id,
+            @RequestBody Product product) {
+
+        log.info("Controller: Received request to update product {}", id);
+
+        Product updatedProduct = productService.update(product, id);
+
+        SuccessResponseDTO<Product> response = SuccessResponseDTO.of(
+                HttpStatus.OK.value(),
+                String.format("Product %d updated successfully", id),
+                updatedProduct
+        );
+
+        log.info("Controller: Product {} updated successfully", id);
+        return ResponseEntity.ok(response);
     }
 
 }
