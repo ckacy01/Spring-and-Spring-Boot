d352d7fd690a832fc9f3c633d2be98222e1ed293
ckacy01 <ckacy01@protonmail.com>
2025-10-18 13:20:52 -0600

    feat: Adding new methods to the MapperUtil.java

--- Meli-E-commerce/src/main/java/org/technoready/meliecommerce/util/MapperUtil.java
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/util/MapperUtil.java
@@ -1,12 +1,16 @@
 package org.technoready.meliecommerce.util;
 
+import org.technoready.meliecommerce.dto.OrderDetailsResponseDTO;
+import org.technoready.meliecommerce.dto.OrderResponseDTO;
 import org.technoready.meliecommerce.dto.ProductDTO;
+import org.technoready.meliecommerce.dto.UserDTO;
+import org.technoready.meliecommerce.entity.Order;
 import org.technoready.meliecommerce.entity.Product;
+import org.technoready.meliecommerce.entity.User;
 
 public class MapperUtil {
 
     public static ProductDTO toDTO(Product entity){
-        new ProductDTO();
         return ProductDTO.builder()
                 .id(entity.getId())
                 .name(entity.getName())
@@ -18,7 +22,6 @@ public class MapperUtil {
     }
 
     public static Product toEntity(ProductDTO dto){
-        new Product();
         return Product.builder()
                 .id(dto.getId())
                 .name(dto.getName())
@@ -26,6 +29,46 @@ public class MapperUtil {
                 .description(dto.getDescription())
                 .created_at(dto.getCreated_at())
                 .active(dto.isActive())
+                .build();
+    }
+
+    public static UserDTO toDTO(User user){
+        return UserDTO.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .createDate(user.getCreateDate())
+                .active(user.isActive())
+                .build();
+    }
+
+    public static User toEntity(UserDTO dto){
+        return User.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .lastName(dto.getLastName())
+                .email(dto.getEmail())
+                .createDate(dto.getCreateDate())
+                .active(dto.isActive())
+                .build();
+    }
+
+    public static OrderResponseDTO toDTO(Order order){
+        return OrderResponseDTO.builder()
+                .id(order.getId())
+                .userId(order.getUser().getId())
+                .createdAt(order.getCreatedAt())
+                .total(order.getTotal())
+                .details(order.getDetails().stream()
+                        .map(d -> OrderDetailsResponseDTO.builder()
+                                .productId(d.getProduct().getId())
+                                .productName(d.getProductName())
+                                .descriptionSnap(d.getDescriptionSnap())
+                                .quantity(d.getQuantity())
+                                .unitPrice(d.getUnitPrice())
+                                .build())
+                        .toList())
                 .build();
     }
 }
