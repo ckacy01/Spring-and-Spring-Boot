b8c5f1cf83ed0cfddb445e3b01ae363d7b890187
ckacy01 <ckacy01@protonmail.com>
2025-10-18 15:36:06 -0600

    refactor: Adding logger and exception handler and also improving the name of the variables

--- Meli-E-commerce/src/main/java/org/technoready/meliecommerce/service/UserService.java
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/service/UserService.java
@@ -1,49 +1,83 @@
 package org.technoready.meliecommerce.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.technoready.meliecommerce.entity.User;
+import org.technoready.meliecommerce.exception.ResourceNotFoundException;
 import org.technoready.meliecommerce.repository.UserRepository;
 
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public List<User> findAll() {
-        return userRepository.findAll();
+        log.info("Retrieving all users");
+        List<User> users = userRepository.findAll();
+        log.info("Retrieved {} users", users.size());
+        return users;
     }
 
     public List<User> findAllIsActive() {
-        return userRepository.findAllByActiveTrue();
+        log.info("Retrieving all active users");
+        List<User> users = userRepository.findAllByActiveTrue();
+        log.info("Retrieved {} active users", users.size());
+        return users;
     }
 
     public User findById(Long id) {
-        return userRepository.findById(id).orElse(null);
+        log.info("Retrieving user with id: {}", id);
+        return userRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("User not found with id: {}", id);
+                    return new ResourceNotFoundException("User", "id", id);
+                });
     }
 
     public User save(User user) {
-        return userRepository.save(user);
+        log.info("Creating new user: {}", user.getEmail());
+        User savedUser = userRepository.save(user);
+        log.info("User created successfully with id: {}", savedUser.getId());
+        return savedUser;
     }
 
     public User update(User user, Long id) {
-        User user1 = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not Found!") );
-        user1.setEmail(user.getEmail());
-        user1.setName(user.getName());
-        user1.setLastName(user.getLastName());
-        user1.setActive(user.isActive());
-        return userRepository.save(user1);
+        log.info("Attempting to update user with id: {}", id);
+
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Cannot update - User not found with id: {}", id);
+                    return new ResourceNotFoundException("User", "id", id);
+                });
+
+        existingUser.setEmail(user.getEmail());
+        existingUser.setName(user.getName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setActive(user.isActive());
+
+        User updatedUser = userRepository.save(existingUser);
+        log.info("User with id: {} has been successfully updated", id);
+
+        return updatedUser;
     }
 
     public void delete(Long id) {
-        User  user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not Found!") );
+        log.info("Attempting to delete user with id: {}", id);
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Cannot delete - User not found with id: {}", id);
+                    return new ResourceNotFoundException("User", "id", id);
+                });
+
         user.setActive(false);
         userRepository.save(user);
+        log.info("User with id: {} has been successfully deactivated", id);
     }
-
 }
