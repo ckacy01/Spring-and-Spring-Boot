77899fc34fa1150f587288d7cc45c5d31e06b61c
ckacy01 <ckacy01@protonmail.com>
2025-10-18 14:53:41 -0600

    feat: Adding methods to find different things

--- Meli-E-commerce/src/main/java/org/technoready/meliecommerce/repository/OrderRepository.java
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/repository/OrderRepository.java
@@ -3,4 +3,10 @@ package org.technoready.meliecommerce.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.technoready.meliecommerce.entity.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(long id);
+    List<Order> findByUser_IdAndActiveTrue(Long userId);
+    List<Order> findOrdersByActiveTrue();
+}
