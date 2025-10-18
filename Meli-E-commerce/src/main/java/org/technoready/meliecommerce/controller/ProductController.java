864327647e33d138176bf398f5a38b98dc700b8b
ckacy01 <ckacy01@protonmail.com>
2025-10-18 15:49:03 -0600

    fix: Removing unused imports

--- Meli-E-commerce/src/main/java/org/technoready/meliecommerce/controller/ProductController.java
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/controller/ProductController.java
@@ -1,7 +1,6 @@
 package org.technoready.meliecommerce.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.technoready.meliecommerce.entity.Product;
--- Meli-E-commerce/src/main/java/org/technoready/meliecommerce/service/ProductService.java
+++ Meli-E-commerce/src/main/java/org/technoready/meliecommerce/service/ProductService.java
@@ -2,7 +2,6 @@ package org.technoready.meliecommerce.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.technoready.meliecommerce.entity.Product;
 import org.technoready.meliecommerce.exception.ResourceNotFoundException;
