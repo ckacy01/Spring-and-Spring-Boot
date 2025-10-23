1f5ccbb05d52571bcbb4e863035361019e914b26
ckacy01 <ckacy01@protonmail.com>
2025-10-23 10:57:16 -0600

    fix: removing unused import

--- Meli-E-commerce/src/test/java/org/technoready/meliecommerce/integration/OrderControllerIntegrationTest.java
+++ Meli-E-commerce/src/test/java/org/technoready/meliecommerce/integration/OrderControllerIntegrationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
